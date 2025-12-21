7bcee7744b5300491f8c7800832f23e1b2bab5d3
HariSreedeth <z5521446@ad.unsw.edu.au>
2025-12-22 00:27:21 +1100

    build v1

---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html',
+    './index.tsx',
+    './App.tsx',
+    './components/**/*.{ts,tsx}',
+    './pages/**/*.{ts,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
